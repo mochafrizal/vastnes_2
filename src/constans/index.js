ae5edcad12a747a1b56a4b0993561f993990f267
mochafrizal <mochafrizalm60@gmail.com>
2025-10-30 22:02:54 +0700

    initial commit

---
+++ src/constans/index.js
@@ -0,0 +1,12 @@
+export const navLinks = [
+
+    { label: 'store' },
+    { label: 'mac' },
+    { label: 'iphone' },
+    { label: 'watch' },
+    { label: 'vision' },
+    { label: 'airpods' },
+
+]
+
+
